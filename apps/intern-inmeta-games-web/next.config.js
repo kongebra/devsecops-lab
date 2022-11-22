8a84404c265a6377d33a8b63a66783d8a37ca97f
Svein Are Danielsen <svein.danielsen@inmeta.no>
2022-11-23 00:26:29 +0100

    refactor: change db url env name

--- apps/intern-inmeta-games-web/next.config.js
+++ apps/intern-inmeta-games-web/next.config.js
@@ -3,12 +3,6 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   output: "standalone",
-
-  env: {
-    DATABASE_URL:
-      process.env.AZURE_APP_CONFIG_CONNECTION_STRING ||
-      process.env.DATABASE_URL,
-  },
 };
 
 module.exports = nextConfig;
